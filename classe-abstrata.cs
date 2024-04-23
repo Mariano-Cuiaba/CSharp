89c8db232c8d725caffc02352d77346bd843f615
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-04-23 14:29:00 -0400

    atividade abstract class

--- classe-abstrata.cs
+++ classe-abstrata.cs
@@ -2,102 +2,123 @@ using System;
 
 public abstract class Samurais
 {
-    //Atributos da classe Samurais
-    public string nome;
-    public int honra;
-    public int forca;
+    private string nome;
+    private int honra;
+    private int forca;
 
-    // Construtor da classe Samurai
-    public Samurai(string nome, int honra, int forca)
+    public Samurais(string nome, int honra, int forca)
     {
         this.nome = nome;
         this.honra = honra;
         this.forca = forca;
     }
-    // Getter para o nome 
-    public string GetNome(){
+
+    public string GetNome()
+    {
         return nome;
     }
-    // Setter para o nome
-    public void SetNome(string nome){
+
+    public void SetNome(string nome)
+    {
         this.nome = nome;
     }
-    // Getter para a honra
-    public string GetHonra(){
+
+    public int GetHonra()
+    {
         return honra;
     }
-    // Setter para a honra
-    public void SetHonra(string honra){
+
+    public void SetHonra(int honra)
+    {
         this.honra = honra;
     }
-    // Getter para o força
-    public string GetForca(){
+
+    public int GetForca()
+    {
         return forca;
     }
-    // Setter para o força
-    public void SetForca(string forca){
+
+    public void SetForca(int forca)
+    {
         this.forca = forca;
     }
-using System;
-    public abstract class Samurais {
-    public string Nome { get; set; }
-    public int Honra { get; set; }
-    public int Forca { get; set; }
-    public Samurais(string nome, int honra, int forca) {
-        Nome = nome;
-        Honra = honra;
-        Forca = forca;
-    }
-    public abstract void Golpear(); // Metodo abstrato
-    public abstract void Defender(); // Metodo abstrato
-    public void GanharHonra(int honra) { // Metodo concreto
-        Honra -= honra;
-        Console.WriteLine($"{Nome} ganhou {honra} honra e agora tem {honra} de honra.");
-        if (Honra <= 0) {
+
+    public abstract void Golpear();
+    public abstract void Defender();
+
+    public void GanharHonra(int honra)
+    {
+        this.honra += honra;
+        Console.WriteLine($"{nome} ganhou {honra} honra e agora tem {this.honra} de honra.");
+        if (this.honra <= 0)
+        {
             Suicidio();
         }
     }
-    public void Suicidio() { // Metodo concreto
-        Console.WriteLine($"{Nome} teve que se suicidar por causa da honra baixa!");
-    }
-}
-public class Goshi : Personagem {
-    public Goshi(string nome, int honra, int forca) : base(nome, honra, forca) {}
-    public override void Golpear() {
-        Console.WriteLine($"{Nome} golpeia com sua foice!");
-    }
-    public override void Defender() {
-        Console.WriteLine($"{Nome} defende com seu corpo!");
-    }
-}
-public class Hatamoto : Personagem {
-    public Hatamoto(string nome, int honra, int forca) : base(nome, honra, forca) {}
 
-    public override void Golpear() {
-        Console.WriteLine($"{Nome} golpeia com a sua katana!");
-    }
-    public override void Defender() {
-        Console.WriteLine($"{Nome} defende com seu corpo!");
+    public void Suicidio()
+    {
+        Console.WriteLine($"{nome} teve que se suicidar por causa da honra baixa!");
     }
 }
-public class Gonekin : Personagem {
-    public Gonekin(string nome, int honra, int forca) : base(nome, honra, forca) {}
 
-    public override void Golpear() {
-        Console.WriteLine($"{Nome} golpeia com sua wakizashi!");
+public class Goshi : Samurais
+{
+    public Goshi(string nome, int honra, int forca) : base(nome, honra, forca) { }
+
+    public override void Golpear()
+    {
+        Console.WriteLine($"{GetNome()} golpeia com sua foice!");
     }
-    public override void Defender() {
-        Console.WriteLine($"{Nome} defende com seu corpo!");
+
+    public override void Defender()
+    {
+        Console.WriteLine($"{GetNome()} defende com seu corpo!");
     }
 }
-public class Sensei : Personagem {
-    public Sensei(string nome, int honra, int forca) : base(nome, honra, forca) {}
 
-    public override void Golpear() {
-        Console.WriteLine($"{Nome} golpeia com seu bastão!");
+public class Hatamoto : Samurais
+{
+    public Hatamoto(string nome, int honra, int forca) : base(nome, honra, forca) { }
+
+    public override void Golpear()
+    {
+        Console.WriteLine($"{GetNome()} golpeia com a sua katana!");
     }
-    public override void Defender() {
-        Console.WriteLine($"{Nome} defende com seu corpo!");
+
+    public override void Defender()
+    {
+        Console.WriteLine($"{GetNome()} defende com seu corpo!");
+    }
+}
+
+public class Gonekin : Samurais
+{
+    public Gonekin(string nome, int honra, int forca) : base(nome, honra, forca) { }
+
+    public override void Golpear()
+    {
+        Console.WriteLine($"{GetNome()} golpeia com sua wakizashi!");
+    }
+
+    public override void Defender()
+    {
+        Console.WriteLine($"{GetNome()} defende com seu corpo!");
+    }
+}
+
+public class Sensei : Samurais
+{
+    public Sensei(string nome, int honra, int forca) : base(nome, honra, forca) { }
+
+    public override void Golpear()
+    {
+        Console.WriteLine($"{GetNome()} golpeia com seu bastão!");
+    }
+
+    public override void Defender()
+    {
+        Console.WriteLine($"{GetNome()} defende com seu corpo!");
     }
 }
 
@@ -111,14 +132,12 @@ class Program
         Samurais sensei = new Sensei("Hanzo", 1000, 1400);
 
         goshi.Golpear();
-        ((Goshi)goshi).Defender(); 
+        goshi.Defender();
         hatamoto.Golpear();
-        ((Hatamoto)hatamoto).Defender();
+        hatamoto.Defender();
         gonekin.Golpear();
-        ((Gonekin)gonekin).Defender();
+        gonekin.Defender();
         sensei.Golpear();
-        ((Sensei)sensei).Defender();
-        
+        sensei.Defender();
     }
-}
 }
